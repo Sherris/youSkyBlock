580bd9acc6d5cece1a26c62a271a2b01e617ba54
Sherris <evergreen22>
2018-04-27 14:53:52 -0400

    Implemented basic file saving code

--- src/main/java/com/farmsoft/youskyblock/commands/ChallengeCommand.java
+++ src/main/java/com/farmsoft/youskyblock/commands/ChallengeCommand.java
@@ -1,5 +1,6 @@
 package com.farmsoft.youskyblock.commands;
 
+import com.farmsoft.youskyblock.player.PlayerInfo;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
@@ -8,6 +9,9 @@ public class ChallengeCommand {
 
     public static void execute(MinecraftServer server, EntityPlayerMP player, String[] args) {
         player.sendMessage((new TextComponentString("\u00a7cTemp Msg - opening Challenges Command")));
+        PlayerInfo pi = new PlayerInfo(player);
+        pi.die();
+        player.sendMessage((new TextComponentString("dead")));
 
     }
 }
--- src/main/java/com/farmsoft/youskyblock/player/PlayerInfo.java
+++ src/main/java/com/farmsoft/youskyblock/player/PlayerInfo.java
@@ -2,28 +2,28 @@ package com.farmsoft.youskyblock.player;
 
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagFloat;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PlayerInfo implements Serializable {
+public class PlayerInfo implements Serializable, IPlayerInfo {
 
     private BlockPos islandCenter;
     private BlockPos islandHome;
     private Map<String, Integer> challengeMap;  //String challenge ID, Integer: times completed
     private int deaths;
-    private EntityPlayerSP player;
+    private EntityPlayerMP player;
 
 
-    public PlayerInfo(EntityPlayerSP playerSP) {
+    public PlayerInfo(EntityPlayerMP playerMP) {
         if (isNew()) {
-            player = playerSP;
+            player = playerMP;
             deaths = 0;
             challengeMap = new HashMap<>();
             islandCenter = new BlockPos(0,255, 0);
@@ -31,11 +31,41 @@ public class PlayerInfo implements Serializable {
             save();
         } else {
             //read player data from file
+            load();
+
         }
     }
 
+    public void setHome(BlockPos homePos) {
+
+    }
+
+    public BlockPos getHome() {
+        return islandHome;
+    }
+
+    public void completeChallenge(String challengeName){
+        challengeMap.put(challengeName, challengeMap.getOrDefault(challengeName,0)+1);
+    };
+
+    public Map<String, Integer> getChallenges(){
+        return challengeMap;
+    }
+
+    public void die () {
+        deaths++;
+        save();
+    }
+
+    public int getDeaths(){
+        return deaths;
+    };
+
+
     public void save() {
         //Save PlayerInfo to file
+
+
         FileOutputStream fout = null;
         ObjectOutputStream oos = null;
 
@@ -72,13 +102,50 @@ public class PlayerInfo implements Serializable {
         }
     }
 
-    public void onDeath () {
-        deaths++;
-        save();
+    private void load() {
+        PlayerInfo pi = null;
+
+        FileInputStream fin = null;
+        ObjectInputStream ois = null;
+
+        try {
+
+            fin = new FileInputStream("C:\\Work\\MC_Modding\\apr-21\\run\\config\\PlayerInfo.ser");
+            ois = new ObjectInputStream(fin);
+            pi = (PlayerInfo) ois.readObject();
+            islandCenter = pi.islandCenter;
+            islandHome = pi.islandHome;
+            challengeMap = pi.challengeMap;
+            deaths = pi.deaths;
+            player = pi.player;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+
+            if (fin != null) {
+                try {
+                    fin.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (ois != null) {
+                try {
+                    ois.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
     }
+
 
     private boolean isNew() {
         //check if file exists for player
-        return true;
+        return false;
     }
 }
